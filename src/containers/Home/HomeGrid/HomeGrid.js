0509f40d22c2f687fd0adfcedc6a329a29870c03
Adrian Delgado <katxrezzam@hotmail.com>
2022-12-31 04:12:52 -0500

    Working on home

---
+++ src/containers/Home/HomeGrid/HomeGrid.js
@@ -0,0 +1,15 @@
+import './HomeExplain.scss'
+const HomeExplain = () => {
+    return(
+        <section className={'explain'}>
+            <div className="width-wrapper">
+                <div className="dino-grid">
+                    <div className="row">
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default HomeExplain
